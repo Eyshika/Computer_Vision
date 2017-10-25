a3961c7caea3082bdd992270283f96a2bc98ac09
Eyshika Agarwal <eyshikaengineer@gmail.com>
2017-10-25 14:23:41 -0400

    Create README.m

---
+++ Kmeans/README.m
@@ -0,0 +1,9 @@
+K-Means is a clustering algorithm. That means you can "group" points based on their neighbourhood.
+When a lot of points a near by, you mark them as one cluster. With K-means, you can find good center points for these clusters.
+
+This "clustering" is not limited to two dimensions. You can have points in 3 dimensions. Or any number of dimensions you want.
+
+The aim of this project was too apply Kmeans segmentation on white-tower image for different k values.
+For initialization I randomly picked 10 RGB triplets.
+
+
